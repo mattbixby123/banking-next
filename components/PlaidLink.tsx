0b4263887b909da26b346737c1bd0d5ef70b0a15
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-08 18:39:16 -0400

    Redirect to / after linking a bank account with Plain.. hurraygit add --all

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,38 +1,41 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
-import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link';
-import { StyledString } from 'next/dist/build/swc';
-import { useRouter } from 'next/navigation';
-import { createLinkToken } from '@/lib/actions/user.actions';
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
+import { useRouter } from 'next/navigation'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
-
   const router = useRouter();
   const [token, setToken] = useState('');
 
   useEffect(() => {
     const getLinkToken = async () => {
-      const data = await createLinkToken(user);
-      setToken(data?.linkToken);
+      try {
+        const data = await createLinkToken(user)
+        setToken(data?.linkToken)
+      } catch (error) {
+        console.error('Error creating link token:', error)
+      }
     }
 
-    getLinkToken();
-  }, [user]);
+    getLinkToken()
+  }, [user])
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
-    await exchangePublicToken({
-      publicToken: public_token,
-      user,
-    })
-    router.push('/');
-  }, [user]);
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      })
+      router.push('/')
+    
+  }, [user])
 
   const config: PlaidLinkOptions = {
     token,
     onSuccess
   }
 
-  const { open, ready } = usePlaidLink(config);
+  const { open, ready } = usePlaidLink(config)
   
   return (
     <>
@@ -44,12 +47,12 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
         >
           Connect bank
         </Button>
-      ): variant === 'ghost' ? (
-        <Button>
+      ) : variant === 'ghost' ? (
+        <Button variant="ghost" onClick={() => open()} disabled={!ready}>
           Connect bank
         </Button>
-      ): (
-        <Button>
+      ) : (
+        <Button onClick={() => open()} disabled={!ready}>
           Connect bank
         </Button>
       )}
