e666136697f6474e9f4bc0b030525346a2e8b04e
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-09 21:20:51 -0400

    second plaid-sandbox-chase account added, this time savings, now both show on the dashboard. next step is to set up the transactions Link on the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 
 const Sidebar = ({ user }: SiderbarProps) => {
@@ -52,7 +53,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
           )
         })}
 
-        {/* USER */}
+        <PlaidLink user={user}/>
       </nav>
       <Footer user={user} />
     </section>
--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -8,9 +8,24 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from '@/lib/utils'
+import { transactionCategoryStyles } from "@/constants"
+import { cn, formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from '@/lib/utils'
 
-
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const {
+    borderColor,
+    backgroundColor,
+    textColor,
+    chipBackgroundColor,
+   } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles.default
+   
+  return (
+    <div className={cn('category-badge', borderColor, chipBackgroundColor)}>
+      <div className={cn('size-2 rounded-full', backgroundColor)} />
+      <p className={cn('text-[12px] font-medium', textColor)}>{category}</p>
+    </div>
+  )
+}
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
     <Table>
@@ -52,7 +67,7 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
               </TableCell>
 
               <TableCell className="pl-2 pr-10">
-                {/* <CategoryBadge category={status} />  */}
+                <CategoryBadge category={status} /> 
               </TableCell>
 
               <TableCell className="min-w-32 pl-2 pr-10">
@@ -64,7 +79,7 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
               </TableCell>
 
               <TableCell className="pl-2 pr-10 max-md:hidden">
-               {/* <CategoryBadge category={t.category} />  */}
+                <CategoryBadge category={t.category} /> 
               </TableCell>
             </TableRow>
           )
