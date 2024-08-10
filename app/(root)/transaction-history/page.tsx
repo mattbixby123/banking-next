d9573bfe733913f069c14c940528175302f16057
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-10 13:52:51 -0400

    finalize the app

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
@@ -16,6 +17,15 @@ const TransactionHistory = async ({ searchParams: { id, page }}: SearchParamProp
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId; 
 
   const account = await getAccount({ appwriteItemId });
+
+
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage );
+
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction)
 
   return (
     <section className='transactions'>
@@ -48,8 +58,16 @@ const TransactionHistory = async ({ searchParams: { id, page }}: SearchParamProp
 
         <section className='flex w-full flex-col gap-6'>
           <TransactionsTable 
-            transactions={account?.transactions}
+            transactions={currentTransactions}
           />
+          {totalPages > 1 && (
+            <div className="my-4 w-full">
+              <Pagination 
+                totalPages={totalPages}
+                page={currentPage}
+              />
+            </div>
+          )}
         </section>
       </div>
     </section>
